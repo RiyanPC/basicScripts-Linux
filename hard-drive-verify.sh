a4170d772a486be8a9ca03b95f2ae1ffe73313cd
Rider <locochon56303@gmail.com>
2024-11-18 16:49:17 -0500

    Update

--- hard-drive-verify.sh
+++ hard-drive-verify.sh
@@ -6,13 +6,13 @@ THRESHOLD=1
 LOG_FILE="./disco-duro.log"
 
 # Funcion para verificar el espacio del disco
-verificar_espacio(){
+verify_sp(){
 
 USO=$(df / |grep / |awk '{print $5}'| sed 's/%//')
 
 # Sentencias para comparar uso del disco
 if [ "$USO" -gt "$THRESHOLD" ]; then
-ALERT="ALERTA: El uso del disco duro ha superado ${THRESHOLD}%"
+ALERT="ALERT: Hard drive space has been exceeded ${THRESHOLD}%"
 
 echo "$ALERT"
 echo "$(date): $ALERT" >> "$LOG_FILE"
@@ -21,4 +21,4 @@ fi
 
 }
 
-verificar_espacio
+verify_sp
